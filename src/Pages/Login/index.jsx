071ad6e21ad080f7b87b61b398e6f1ed05f11468
camotio02 <temotio.bernardo@snowmanlabs.com>
2022-12-23 12:52:58 -0300

    Definindo e controlando os dados

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,19 +1,46 @@
-import { Button, Card, colors } from '@mui/material'
+import { Button, Card, colors, Dialog, DialogContent, Stack } from '@mui/material'
 import './index.css'
 import {
     LogoMarca
 } from '../../conponent/LogoMarca/index'
 import { InputConponent } from '../../conponent/Input'
-import {
-    ButtonsConponent
-} from '../../conponent/Buttons/index'
 import { ComponentID } from '../Catasdro/componentID'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 export const Login = () => {
-    const Entrando = () => {
-        alert('OK')
+    const [showErroDinamic, setShowErroDinamic] = useState(false)
+    const navigate = useNavigate()
+    const momentSendDatas = {
+        primeiro: 'Verificando se seus dados existem...',
+        segundo: 'Enviando seus dados para o servidor...',
+        terceiro: 'Retornando uma resposta...',
+        quarto: 'Concluindo as etapas e entrando...'
     }
+    function etapas() {
+        setTimeout(() => {
+            setShowErroDinamic(`${momentSendDatas.primeiro}`)
+        }, "1000");
+        setTimeout(() => {
+            setShowErroDinamic(`${momentSendDatas.segundo}`)
+        }, "3000");
+        setTimeout(() => {
+            setShowErroDinamic(`${momentSendDatas.terceiro}`)
+        }, "6000");
+        setTimeout(() => {
+            setShowErroDinamic(`${momentSendDatas.quarto}`)
+        }, "8000");
+        setTimeout(() => {
+            setShowErroDinamic("");
+            setShowErroDinamic(false);
+            navigate('/dashboard')
+        }, "10000");
+    }
+    const Entrando = () => {
+        let newMessage = "ERRO:\n";
+        etapas()
+     
+    }
+
     const [data, setData] = useState(
         {
             nameSchool: '',
@@ -119,6 +146,14 @@ export const Login = () => {
                     </Link>
                 </Button>
             </div>
+            <Dialog open={showErroDinamic} >
+                <DialogContent>
+                    <Stack>
+                        <div style={{ color: "red" }}>{showErroDinamic}</div>
+                        <div className="loader--text"></div>
+                    </Stack>
+                </DialogContent>
+            </Dialog>
         </>
     )
 }
