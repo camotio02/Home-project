22efe5fcd85b22f0cb76db2222edcb65129ea202
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2023-01-12 16:40:44 -0300

    mexendo com a estrura do logo

--- src/conponent/Logo/logo-svg.jsx
+++ src/conponent/Logo/logo-svg.jsx
@@ -1,7 +1,11 @@
 import './logo-svg.css'
-export const Logo = () => <><h1 className="h1-logo">
+export const Logo = () => <>
+<div className='logos'>
+<h1 className="h1-logo">
     <strong>E</strong>
     <strong>C</strong>
     <strong>S</strong>
 </h1>
+<span>School</span>
+</div>
 </>
