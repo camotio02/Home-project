9ee757daf99ea97f7f5e14163e1c23b4edf2a006
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2023-01-25 16:24:30 -0300

    cria conponente para mostar a frequência do aluno

---
+++ src/USUARIOLOGADO/AcessoDoAluno/conponentAcessoAluno/FREQUENCIA/frequencia.jsx
@@ -0,0 +1,23 @@
+import './frequencia.css'
+export const FrequenciaConponent = () => {
+    return (
+        <>
+            <div className="line">
+                <span>323</span>
+                <span>Presenças</span>
+            </div>
+            <div className="line">
+                <span>92</span>
+                <span>Ausências</span>
+            </div>
+            <div className="line">
+                <span>462</span>
+                <span>Aulas</span>
+            </div>
+            <div className="line">
+                <span>25%</span>
+                <span>Faltas atingidos</span>
+            </div>
+        </>
+    )
+}
