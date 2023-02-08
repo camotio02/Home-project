56b34d71dbaf2b8032605ff1b18670d9a8b30ffb
Temotio Luis <temotio.bernardo@snowmanlabs.com>
2023-02-08 15:07:00 -0300

    card das informação

--- src/USUARIOLOGADO/AcessoProfessor/pages/Dashboard/HomePage.jsx
+++ src/USUARIOLOGADO/AcessoProfessor/pages/Dashboard/HomePage.jsx
@@ -1,16 +1,23 @@
 import { LinksProfConpinent } from '../../conponentAcessoProfessor/LINKSPROF/linkProf'
 import './HomePage.css'
 import { Card } from '@mui/material';
+import { useState } from 'react';
 
-export const HomePageProf = () => {
+
+export const HomeTeacher = () => {
+   
     return (
         <>
             <div className="containerProf">
                 <div className="logoProf">
                     <h4>Olá prof, Fullano de tall...</h4>
                 </div>
+                <Card className='centerInfoProf'>
+                    Agora
+                </Card>
+                
                 <Card className="linksProf">
-                    <LinksProfConpinent/>
+                    <LinksProfConpinent />
                 </Card>
             </div>
         </>
